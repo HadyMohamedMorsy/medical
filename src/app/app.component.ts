40e6237c9f7dddc328f9a91bb61b0b8117f5645a
Hady M. Morsy <72827484+HadyMohamedMorsy@users.noreply.github.com>
2023-03-17 19:36:52 +0200

    standalone-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {SharedModuleModule} from '@shared/shared-module.module';
 
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports : [SharedModuleModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import {importProvidersFrom} from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .catch(err => console.error(err));
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent , {
+  providers : [
+    importProvidersFrom(AppRoutingModule)
+  ]
+})
